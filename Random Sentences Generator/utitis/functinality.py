3191fa1d869d50e402ce8a0879329db0f4161e72
Петър Попов <popovstz@gmail.com>
2023-10-15 21:42:52 +0300

    reaname

--- Random Sentences Generator/utitis/functinality.py
+++ Random Sentences Generator/utitis/functinality.py
@@ -2,43 +2,43 @@ from random import randint
 
 
 def get_random_adverb() -> str:
-    with open(r"Random Sentences Generator\text\adverbs.txt", "r") as text_file:
-        line = text_file.readlines()
+    with open(r"Random Sentences Generator\text\adverbs.txt", "r") as txt_file:
+        line = txt_file.readlines()
         word_choice = randint(1, 999)
     return line[word_choice].strip()
 
 
 def get_random_city() -> str:
-    with open(r"Random Sentences Generator\text\cities.txt", "r") as text_file:
-        line = text_file.readlines()
+    with open(r"Random Sentences Generator\text\cities.txt", "r") as txt_file:
+        line = txt_file.readlines()
         word_choice = randint(1, 79100)
     return line[word_choice].strip()
 
 
 def get_random_detail() -> str:
-    with open(r"Random Sentences Generator\text\details.txt", "r") as text_file:
-        line = text_file.readlines()
+    with open(r"Random Sentences Generator\text\details.txt", "r") as txt_file:
+        line = txt_file.readlines()
         word_choice = randint(1, 124)
     return line[word_choice].strip()
 
 
 def get_random_name() -> str:
-    with open(r"Random Sentences Generator\text\names.txt", "r") as text_file:
-        line = text_file.readlines()
+    with open(r"Random Sentences Generator\text\names.txt", "r") as txt_file:
+        line = txt_file.readlines()
         word_choice = randint(1, 5980)
     return line[word_choice].strip().capitalize()
 
 
 def get_random_noun() -> str:
-    with open(r"Random Sentences Generator\text\nouns.txt", "r") as text_file:
-        line = text_file.readlines()
+    with open(r"Random Sentences Generator\text\nouns.txt", "r") as txt_file:
+        line = txt_file.readlines()
         word_choice = randint(1, 499)
     return line[word_choice].strip()
 
 
 def get_random_verb() -> str:
-    with open(r"Random Sentences Generator\text\verbs.txt", "r") as text_file:
-        line = text_file.readlines()
+    with open(r"Random Sentences Generator\text\verbs.txt", "r") as txt_file:
+        line = txt_file.readlines()
         word_choice = randint(1, 490)
     return line[word_choice].strip()
 
