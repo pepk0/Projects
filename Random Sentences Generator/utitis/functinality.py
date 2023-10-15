c19ca8c96b4fab7e098eb4e1ae2953ff528273d8
Петър Попов <popovstz@gmail.com>
2023-10-15 20:40:00 +0300

    fixed file paths

--- Random Sentences Generator/utitis/functinality.py
+++ Random Sentences Generator/utitis/functinality.py
@@ -2,42 +2,42 @@ from random import randint
 
 
 def get_random_adverb() -> str:
-    with open(r"text\adverbs.txt", "r") as text_file:
+    with open(r"Random Sentences Generator\text\adverbs.txt", "r") as text_file:
         line = text_file.readlines()
         word_choice = randint(1, 999)
     return line[word_choice].strip()
 
 
 def get_random_city() -> str:
-    with open(r"text\cities.txt", "r") as text_file:
+    with open(r"Random Sentences Generator\text\cities.txt", "r") as text_file:
         line = text_file.readlines()
         word_choice = randint(1, 79100)
     return line[word_choice].strip()
 
 
 def get_random_detail() -> str:
-    with open(r"text\details.txt", "r") as text_file:
+    with open(r"Random Sentences Generator\text\details.txt", "r") as text_file:
         line = text_file.readlines()
         word_choice = randint(1, 124)
     return line[word_choice].strip()
 
 
 def get_random_name() -> str:
-    with open(r"text\names.txt", "r") as text_file:
+    with open(r"Random Sentences Generator\text\names.txt", "r") as text_file:
         line = text_file.readlines()
         word_choice = randint(1, 5980)
     return line[word_choice].strip().capitalize()
 
 
 def get_random_noun() -> str:
-    with open(r"text\nouns.txt", "r") as text_file:
+    with open(r"Random Sentences Generator\text\nouns.txt", "r") as text_file:
         line = text_file.readlines()
         word_choice = randint(1, 499)
     return line[word_choice].strip()
 
 
 def get_random_verb() -> str:
-    with open(r"text\verbs.txt", "r") as text_file:
+    with open(r"Random Sentences Generator\text\verbs.txt", "r") as text_file:
         line = text_file.readlines()
         word_choice = randint(1, 490)
     return line[word_choice].strip()
