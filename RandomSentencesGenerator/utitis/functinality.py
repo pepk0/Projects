f4dd8ff09a995290ed70f539630222e9b81f1414
Петър Попов <popovstz@gmail.com>
2023-10-16 19:26:29 +0300

    rename

--- RandomSentencesGenerator/utitis/functinality.py
+++ RandomSentencesGenerator/utitis/functinality.py
@@ -2,42 +2,42 @@ from random import randint
 
 
 def get_random_adverb() -> str:
-    with open(r"Random Sentences Generator\text\adverbs.txt", "r") as txt_file:
+    with open(r"RandomSentencesGenerator\text\adverbs.txt", "r") as txt_file:
         line = txt_file.readlines()
         word_choice = randint(1, 999)
     return line[word_choice].strip()
 
 
 def get_random_city() -> str:
-    with open(r"Random Sentences Generator\text\cities.txt", "r") as txt_file:
+    with open(r"RandomSentencesGenerator\text\cities.txt", "r") as txt_file:
         line = txt_file.readlines()
         word_choice = randint(1, 79100)
     return line[word_choice].strip()
 
 
 def get_random_detail() -> str:
-    with open(r"Random Sentences Generator\text\details.txt", "r") as txt_file:
+    with open(r"RandomSentencesGenerator\text\details.txt", "r") as txt_file:
         line = txt_file.readlines()
         word_choice = randint(1, 124)
     return line[word_choice].strip()
 
 
 def get_random_name() -> str:
-    with open(r"Random Sentences Generator\text\names.txt", "r") as txt_file:
+    with open(r"RandomSentencesGenerator\text\names.txt", "r") as txt_file:
         line = txt_file.readlines()
         word_choice = randint(1, 5980)
     return line[word_choice].strip().capitalize()
 
 
 def get_random_noun() -> str:
-    with open(r"Random Sentences Generator\text\nouns.txt", "r") as txt_file:
+    with open(r"RandomSentencesGenerator\text\nouns.txt", "r") as txt_file:
         line = txt_file.readlines()
         word_choice = randint(1, 499)
     return line[word_choice].strip()
 
 
 def get_random_verb() -> str:
-    with open(r"Random Sentences Generator\text\verbs.txt", "r") as txt_file:
+    with open(r"RandomSentencesGenerator\text\verbs.txt", "r") as txt_file:
         line = txt_file.readlines()
         word_choice = randint(1, 490)
     return line[word_choice].strip()
