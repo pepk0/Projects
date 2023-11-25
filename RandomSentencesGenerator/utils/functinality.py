b3a5a6b68b75a52aca284f04d74d74c1fd10292e
pepk0 <popovstz@gmail.com>
2023-11-25 10:20:30 +0200

    refactored

--- RandomSentencesGenerator/utils/functinality.py
+++ RandomSentencesGenerator/utils/functinality.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 def get_random_word(path: str) -> str:
-    with open(path, "r") as txt_file:
+    with open(f"RandomSentencesGenerator\\text\\{path}", "r") as txt_file:
         lines = txt_file.readlines()
         last_word = len(lines) - 1
         word_choice = randint(0, last_word)
@@ -12,12 +12,12 @@ def get_random_word(path: str) -> str:
 def make_sentence() -> str:
     sentence = ""
     file_paths = [
-        r"RandomSentencesGenerator\text\names.txt",
-        r"RandomSentencesGenerator\text\cities.txt",
-        r"RandomSentencesGenerator\text\adverbs.txt",
-        r"RandomSentencesGenerator\text\verbs.txt",
-        r"RandomSentencesGenerator\text\nouns.txt",
-        r"RandomSentencesGenerator\text\details.txt"
+        "names.txt",
+        "cities.txt",
+        "adverbs.txt", 
+        "verbs.txt", 
+        "nouns.txt",
+        "details.txt"
     ]
 
     for index, file_path in enumerate(file_paths):
